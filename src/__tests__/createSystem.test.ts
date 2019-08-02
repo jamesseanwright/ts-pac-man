d61de0f71cf23bd9616276a5797246bbfad478bf
James Wright <james@jamesswright.co.uk>
2019-08-02 11:25:30 +0100

    Adds system creator

---
+++ src/__tests__/createSystem.test.ts
@@ -0,0 +1,22 @@
+import createSystem from '../system';
+
+interface TestComponent {
+  i: number;
+}
+
+describe('createSystem', () => {
+  it('should create a system that calls a delegate update function for each provided component', () => {
+    const next = jest.fn();
+    const components = Array(5).fill(null).map((_, i) => ({ i }));
+    const system = createSystem<TestComponent>(components, next);
+    const time = 2000;
+
+    system(time);
+
+    expect(next).toHaveBeenCalledTimes(components.length);
+
+    components.forEach(component => {
+      expect(next).toHaveBeenCalledWith(time, component);
+    });
+  });
+});
---
+++ src/system.ts
@@ -0,0 +1,12 @@
+import { Component } from './component';
+
+export type Next<T extends Component> = (time: number, component: T) => void;
+
+const createSystem = <T extends Component>(components: T[], next: Next<T>) =>
+  (time: number) => {
+    for (let component of components) {
+      next(time, component);
+    }
+  };
+
+export default createSystem;
