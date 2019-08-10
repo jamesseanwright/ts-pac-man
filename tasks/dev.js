869363c5f862dbb4fe8380e215c5680fecb902f7
James Wright <james@jamesswright.co.uk>
2019-08-10 14:49:58 +0100

    Further refines dev runner

--- tasks/dev.js
+++ tasks/dev.js
@@ -9,23 +9,27 @@ const config = require('../rollup.config');
 
 const PORT = 8080;
 
-const watcher = rollup.watch(config);
-
-watcher.on('event', ({ code, duration }) => {
-  if (code === 'BUNDLE_START') {
-    console.log('Changes detected. Bundling...');
-  } else if (code === 'BUNDLE_END') {
-    console.log(`Bundling complete after ${duration} ms`);
-  }
-});
-
 const fileServer = new Server('./dist', {
   'Cache-Control': 'no-cache, must-revalidate'
 });
 
 const serveFile = fileServer.serve.bind(fileServer);
+const server = http.createServer(serveFile);
+const watcher = rollup.watch(config);
 
-http.createServer(serveFile)
-  .listen(PORT, () => {
-    console.log(`File server listening on port ${PORT}...`)
-  });
+watcher.on('event', event => {
+  if (event.code === 'BUNDLE_START') {
+    console.log('Changes detected. Bundling...');
+  } else if (event.code === 'BUNDLE_END') {
+    console.log(`Bundling complete after ${event.duration} ms`);
+  } else if (event.code === 'ERROR') {
+    console.log(`Error thrown! Please fix this and try again:\n${event.error}\n`);
+  } else if (event.code === 'FATAL') {
+    console.error(`Fatal error!\n${event.error}\n`);
+    server.close();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`File server listening on port ${PORT}...`)
+});
