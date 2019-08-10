4130c6583c3b9fdbc15b823fed847e640cfbb767
James Wright <james@jamesswright.co.uk>
2019-08-10 22:01:08 +0100

    Adds keyboard abstraction

---
+++ src/input/__tests__/keyboard.test.ts
@@ -0,0 +1,45 @@
+import createKeyboard from '../keyboard';
+
+describe('keyboard', () => {
+  it('should not mark key as pressed if it is supported but not pressed', () => {
+    const keyboard = createKeyboard(window, ['ArrowUp']);
+
+    expect(keyboard.isKeyPressed('ArrowUp')).toBe(false);
+  });
+
+  it('should mark a key as pressed if it is supported and pressed', () => {
+    const keyboard = createKeyboard(window, ['ArrowUp']);
+
+    window.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'ArrowUp',
+    }));
+
+    expect(keyboard.isKeyPressed('ArrowUp')).toBe(true);
+  });
+
+  it('should not mark a key as pressed if it is not supported', () => {
+    const keyboard = createKeyboard(window, ['ArrowUp']);
+
+    window.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'ArrowDown',
+    }));
+
+    expect(keyboard.isKeyPressed('ArrowDown')).toBe(false);
+  });
+
+  it('should not mark a key as pressed if it is pressed then released', () => {
+    const keyboard = createKeyboard(window, ['ArrowUp']);
+
+    window.dispatchEvent(new KeyboardEvent('keydown', {
+      key: 'ArrowUp',
+    }));
+
+    expect(keyboard.isKeyPressed('ArrowUp')).toBe(true);
+
+    window.dispatchEvent(new KeyboardEvent('keyup', {
+      key: 'ArrowUp',
+    }));
+
+    expect(keyboard.isKeyPressed('ArrowUp')).toBe(false);
+  });
+});
---
+++ src/input/keyboard.ts
@@ -0,0 +1,22 @@
+const createKeyboard = (eventTarget: Window, supportedKeys: string[]) => {
+  const keys = new Map<string, boolean>(
+    supportedKeys.map(key => [key, false]),
+  );
+
+  const updateKey = (key: string, isPressed: boolean) => {
+    if (keys.has(key)) {
+      keys.set(key, isPressed);
+    }
+  };
+
+  eventTarget.addEventListener('keydown', ({ key }) => updateKey(key, true));
+  eventTarget.addEventListener('keyup', ({ key }) => updateKey(key, false));
+
+  return {
+    isKeyPressed(key: string) {
+      return Boolean(keys.get(key));
+    }
+  };
+};
+
+export default createKeyboard;
