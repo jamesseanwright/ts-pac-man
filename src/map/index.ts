fc52c1110dc22c968c1d57dcae2f23e39ac03b0a
James Wright <james@jamesswright.co.uk>
2019-08-12 21:34:44 +0100

    Completes map data

--- src/map/index.ts
+++ src/map/index.ts
@@ -16,13 +16,14 @@ const TILE_HEIGHT = 0.0161;
 /* Numbers next to tile type represent rotation
  * by increments of 1.57 rad/90 deg
  * i.e. 0 => 0, 1 => 1.57, 2 => 3.14, 3 => 4.71 */
+
+// TODO: shorter inner walls for corners
 type OuterCorner = 'A0' | 'A1' | 'A2' | 'A3';
 type InnerCorner = 'B0' | 'B1' | 'B2' | 'B3';
 type StraightWall = 'C0' | 'C1';
 type StraightSingleWall = 'D0' | 'D1' | 'D2' | 'D3';
 type SingleInnerCorner = 'E0' | 'E1' | 'E2' | 'E3';
 type SquareCorner = 'F0' | 'F1' | 'F2' | 'F3';
-type SplittingCorner = 'G0' | 'G1' | 'G2' | 'G3';
 type Gate = 'H';
 type Walkable = 'O';
 
@@ -33,7 +34,6 @@ type Tile =
   | StraightSingleWall
   | SingleInnerCorner
   | SquareCorner
-  | SplittingCorner
   | Gate
   | Walkable;
 
@@ -42,7 +42,7 @@ const fill = (length: number, tile: Tile | Tile[]) => Array(length).fill(tile);
 
 const tiles: Tile[][] = [
   ['A0', ...fill(25, 'C0'), ...fill(4, 'D0'), ...fill(25, 'C0'), 'A1'],
-  ['C1', ...fill(25, 'O'), 'G1', ...fill(2, 'O'), 'G0', ...fill(25, 'O'), 'C1'],
+  ['C1', ...fill(25, 'O'), 'B1', ...fill(2, 'O'), 'B0', ...fill(25, 'O'), 'C1'],
   ...fill(3, ['C1', ...fill(26, 'O'), 'D3', 'D1', ...fill(26, 'O'), 'C1']),
   ['C1', ...fill(4, 'O'), 'B0', ...fill(4, 'D0'), 'B1', ...fill(4, 'O'), 'B0', ...fill(6, 'D0'), 'B1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B0', ...fill(6, 'D0'), 'B1', ...fill(4, 'O'), 'B0', ...fill(4, 'D0'), 'B1', ...fill(4, 'O'), 'C1'],
   ...fill(2, ['C1', ...fill(4, 'O'), 'D3', ...fill(4, 'O'), 'D1', ...fill(4, 'O'), 'D3', ...fill(6, 'O'), 'D1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'D3', ...fill(6, 'O'), 'D1', ...fill(4, 'O'), 'D3', ...fill(4, 'O'), 'D1', ...fill(4, 'O'), 'C1']),
@@ -70,8 +70,17 @@ const tiles: Tile[][] = [
   ['C1', ...fill(4, 'O'), 'B0', ...fill(4, 'D0'), 'B1', ...fill(4, 'O'), 'B0', ...fill(6, 'D0'), 'B1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B0', ...fill(6, 'D0'), 'B1', ...fill(4, 'O'), 'B0', ...fill(4, 'D0'), 'B1', ...fill(4, 'O'), 'C1'],
   ['C1', ...fill(4, 'O'), 'B3', ...fill(2, 'D2'), ...fill(2, 'O'), 'D1', ...fill(4, 'O'), 'B3', ...fill(6, 'D2'), 'B2', ...fill(4, 'O'), 'B3', 'B2', ...fill(4, 'O'), 'B3', ...fill(6, 'D2'), 'B2', ...fill(4, 'O'), 'D3', ...fill(2, 'O'), ...fill(2, 'D2'), 'B2', ...fill(4, 'O'), 'C1'],
   ['C1', ...fill(7, 'O'), 'B1', 'O', 'D1', ...fill(34, 'O'), 'D3', 'O', 'B0', ...fill(7, 'O'), 'C1'],
-  ...fill(3, ['C1', ...fill(8, 'O'), 'D3', 'D1', ...fill(34, 'O'), 'D3', 'D1', ...fill(8, 'O'), 'C1']),
-  ['D3', 'G3']
+  ...fill(2, ['C1', ...fill(8, 'O'), 'D3', 'D1', ...fill(34, 'O'), 'D3', 'D1', ...fill(8, 'O'), 'C1']),
+  ['D3', 'B3', ...fill(7, 'O'), 'D3', 'D1', ...fill(34, 'O'), 'D3', 'D1', ...fill(7, 'O'), 'B2', 'D1'],
+  ['D3', 'O', ...fill(2, 'D0'), 'B1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B0', 'B1', ...fill(4, 'O'), 'B0', ...fill(12, 'D0'), 'B1', ...fill(4, 'O'), 'B0', 'B1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B0', ...fill(2, 'D0'), 'O', 'D1'],
+  ['D3', 'O', ...fill(2, 'D2'), 'B2', ...fill(4, 'O'), 'B3', 'B2', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B3', ...fill(4, 'D2'), ...fill(4, 'O'), ...fill(4, 'D2'), 'B2', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B3', 'B2', ...fill(4, 'O'), 'B3', ...fill(2, 'D2'), 'O', 'D1'],
+  ['D3', 'B0', ...fill(13, 'O'), 'D3', 'D1', ...fill(9, 'O'), 'B1', ...fill(2, 'O'), 'B0', ...fill(9, 'O'), 'D3', 'D1', ...fill(13, 'O'), 'B1', 'D1'],
+  ...fill(2, ['C1', ...fill(14, 'O'), 'D3', 'D1', ...fill(10, 'O'), 'D3', 'D1', ...fill(10, 'O'), 'D3', 'D1', ...fill(14, 'O'), 'C1']),
+  ['C1', ...fill(13, 'O'), 'B2', ...fill(2, 'O'), 'B3', ...fill(9, 'O'), 'D3', 'D1', ...fill(9, 'O'), 'B2', ...fill(2, 'O'), 'B3', ...fill(13, 'O'), 'C1'],
+  ['C1', ...fill(4, 'O'), 'B0', ...fill(8, 'D0'), ...fill(4, 'O'), ...fill(4, 'D0'), 'B1', ...fill(4, 'O'), 'D3', 'D1', ...fill(4, 'O'), 'B0', ...fill(4, 'D0'), ...fill(4, 'O'), ...fill(8, 'D0'), 'B1', ...fill(4, 'O'), 'C1'],
+  ['C1', ...fill(4, 'O'), 'B3', ...fill(16, 'D2'), 'B2', ...fill(4, 'O'), 'B3', 'B2', ...fill(4, 'O'), 'B3', ...fill(16, 'D2'), 'B2', ...fill(4, 'O'), 'C1'],
+  ...fill(4, ['C1', ...fill(54, 'O'), 'C1']),
+  ['A3', ...fill(54, 'C0'), 'A2'],
 ];
 
 // TODO: corners probably need to be walkable...
@@ -83,7 +92,7 @@ const toRadians = (rawRotation: string) => {
 };
 
 // TODO: Should this live under entities?
-// TODO: Test!
+// TODO: Test with injectable map!
 const bindMap = (spriteRenderSystem: System<SpriteRenderable>) => {
   tiles.forEach((rowTiles, row) => {
     rowTiles.forEach((tile, column) => {
