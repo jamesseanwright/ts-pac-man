7e6077376d79e3d22a94624f1f192a9ec406ffc1
James Wright <james@jamesswright.co.uk>
2019-08-17 15:45:12 +0100

    WIP impl of player movement

--- src/input/__tests__/playerMovementSystem.test.ts
+++ src/input/__tests__/playerMovementSystem.test.ts
@@ -1,2 +1,52 @@
 import { createPlayerMovementSystem } from '../playerMovementSystem';
+import createTilePositionable from '../../tilePositionable';
 
+describe('playerMovementSystem', () => {
+  const createKeyboard = (expectedKey: string) => ({
+    isKeyPressed: (key: string) => key === expectedKey,
+  });
+
+  it('should move the player to the column left of its current position when the left arrow is pressed and can move', () => {
+    const tilePositionable = createTilePositionable(2, 1, 1, 1);
+    const keyboard = createKeyboard('ArrowLeft');
+    const canMoveTo = () => true;
+    const playerMovementSystem = createPlayerMovementSystem(keyboard, canMoveTo);
+
+    playerMovementSystem(tilePositionable);
+
+    expect(tilePositionable.pos).toEqual([1, 1]);
+  });
+
+  it('should not move the player to the column left of its current position when the left arrow is pressed and cannot move', () => {
+    const tilePositionable = createTilePositionable(2, 1, 1, 1);
+    const keyboard = createKeyboard('ArrowLeft');
+    const canMoveTo = () => false;
+    const playerMovementSystem = createPlayerMovementSystem(keyboard, canMoveTo);
+
+    playerMovementSystem(tilePositionable);
+
+    expect(tilePositionable.pos).toEqual([2, 1]);
+  });
+
+  it('should move the player to the column right of its current position when the right arrow is pressed and can move', () => {
+    const tilePositionable = createTilePositionable(2, 1, 1, 1);
+    const keyboard = createKeyboard('ArrowRight');
+    const canMoveTo = () => true;
+    const playerMovementSystem = createPlayerMovementSystem(keyboard, canMoveTo);
+
+    playerMovementSystem(tilePositionable);
+
+    expect(tilePositionable.pos).toEqual([3, 1]);
+  });
+
+  it('should not move the player to the column right of its current position when the right arrow is pressed and cannot move', () => {
+    const tilePositionable = createTilePositionable(2, 1, 1, 1);
+    const keyboard = createKeyboard('ArrowRight');
+    const canMoveTo = () => false;
+    const playerMovementSystem = createPlayerMovementSystem(keyboard, canMoveTo);
+
+    playerMovementSystem(tilePositionable);
+
+    expect(tilePositionable.pos).toEqual([2, 1]);
+  });
+});
--- src/input/playerMovementSystem.ts
+++ src/input/playerMovementSystem.ts
@@ -2,9 +2,22 @@
 
 import { Keyboard } from './keyboard';
 import { TilePositionable } from '../tilePositionable';
-import { Tile } from '../map';
+import { canMoveToTile } from '../map';
 
-export const createPlayerMovementSystem = (keyboard: Keyboard, map: Tile[][]) =>
+export const createPlayerMovementSystem = (keyboard: Keyboard, canMoveTo: typeof canMoveToTile) =>
   (component: TilePositionable) => {
-    // if (keyb)
+    const [column, row] = component.pos;
+    let nextColumn = column;
+    let nextRow = row;
+
+    if (keyboard.isKeyPressed('ArrowLeft')) {
+      nextColumn = column - 1;
+    }
+
+    if (canMoveTo(nextColumn, nextRow)) {
+      /* Mutating to avoid GC-related jank
+       * TODO: cover this in presentation */
+      component.pos[0] = nextColumn;
+      component.pos[1] = nextRow;
+    }
   };
