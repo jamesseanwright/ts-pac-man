b1d5f04a7a2d79adeadbee7b57c9d32953ba9edf
James Wright <james@jamesswright.co.uk>
2019-08-19 23:07:31 +0100

    WIP movement elsewhere

--- src/tracking/__tests__/trackingSystem.test.ts
+++ src/tracking/__tests__/trackingSystem.test.ts
@@ -27,6 +27,32 @@ describe('trackingSystem', () => {
 
     trackingSystem(trackingMoveable);
 
-    expect(trackingMoveable.trackerMoveable.direction).toEqual([4, 2]);
+    expect(trackingMoveable.trackerMoveable.direction).toEqual([1, 0]);
+  });
+
+  it.only('should set the tracker`s direction to down when the closest walkable to the target is below', () => {
+    const trackerPositionable = createTilePositionable(3, 2, 1, 1);
+    const trackerMoveable = createMoveable(0, 0, 1, 1);
+    const targetPositionable = createTilePositionable(9, 4, 1, 1);
+
+    const trackingMoveable = createTrackingMoveable(
+      trackerPositionable,
+      trackerMoveable,
+      targetPositionable,
+    );
+
+    /* a stub that emulates
+     * a real map layout */
+    const canMoveTo = (
+      currentPositionable: TilePositionable,
+      column: number,
+      row: number
+    ) => row !== 2 && column === 3;
+
+    const trackingSystem = createTrackingSystem(canMoveTo);
+
+    trackingSystem(trackingMoveable);
+
+    expect(trackingMoveable.trackerMoveable.direction).toEqual([0, 1]);
   });
 });
--- src/tracking/trackingSystem.ts
+++ src/tracking/trackingSystem.ts
@@ -5,17 +5,42 @@ import { TilePositionable } from '../tilePositionable';
 const getNeighbouringTiles = ({ pos: [column, row] }: TilePositionable): [number, number][] =>
   [[column - 1, row], [column + 1, row], [column, row - 1], [column, row + 1]];
 
-const getClosestTileToTarget = (neighbouringTiles: [number, number][], target: TilePositionable): [number, number] =>
-  [-1, -1];
+const getDistance = ([ax, ay]: [number, number], [bx, by]: [number, number]) =>
+  (bx + by) - (ax + ay);
 
-const getDirectionToClosestTile = (tile: [number, number]) =>
-  [-1, -1];
+/* Retrieves the tile with the *smallest*
+ * aggregate distance from the tracker */
+const getClosestTileToTarget = (neighbouringTiles: [number, number][], target: TilePositionable): [number, number] =>
+  neighbouringTiles.sort(
+    (a, b) => getDistance(a, target.pos) - getDistance(b, target.pos)
+  )[0];
+
+/* This acts upon both directions, but
+ * given how getNeighbouringTiles works,
+ * we can safely assume that only one scalar
+ * will be greater than zero.
+ * TODO: find a succinct, mathsy way of
+ * expressing this (I'm tired...) */
+const getDirectionToClosestTile = ({ pos }: TilePositionable, tile: [number, number]) =>
+  pos.map((p, i) => {
+    if (p < tile[i]) {
+      return 1;
+    }
+
+    if (p > tile[i]) {
+      return -1;
+    }
+
+    return 0;
+  }) as [number, number];
 
 export const createTrackingSystem = (canMoveTo: typeof canMoveToTile) =>
   ({ trackerPositionable, trackerMoveable, targetPositionable }: TrackingMoveable) => {
     const neighbouringTiles = getNeighbouringTiles(trackerPositionable);
+    console.log('*******', neighbouringTiles);
     const closestTile = getClosestTileToTarget(neighbouringTiles, targetPositionable);
-    const direction = getDirectionToClosestTile(closestTile);
+    console.log('*******', closestTile);
+    const direction = getDirectionToClosestTile(trackerPositionable, closestTile);
 
     direction.forEach((dir, i) => {
       trackerMoveable.direction[i] = dir;
