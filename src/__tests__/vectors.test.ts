5f6422c0c7b2137bff8e4f94dcf0be4d32c13bad
James Wright <james@jamesswright.co.uk>
2019-09-10 21:38:45 +0100

    Adds unit tests for vector operators

---
+++ src/__tests__/vectors.test.ts
@@ -0,0 +1,35 @@
+import { addVectors, multiplyVectors, ceilingVector } from '../vectors';
+
+describe('vector operations', () => {
+  describe('addVectors', () => {
+    it('should linearly add the vectors it receives as params', () => {
+      const vectors = [
+        [1, 5, 4, 7],
+        [3, 1, 6, 10],
+        [2, 2, 5, -3],
+      ];
+
+      expect(addVectors(...vectors)).toEqual([6, 8, 15, 14]);
+    });
+  });
+
+  describe('multiplyVectors', () => {
+    it('should linearly multiply the vectors it receives as params', () => {
+      const vectors = [
+        [2, 5, 4, 7],
+        [3, 1, 6, 10],
+        [2, 2, 5, -3],
+      ];
+
+      expect(multiplyVectors(...vectors)).toEqual([12, 10, 120, -210]);
+    });
+  });
+
+  describe('ceilingVector', () => {
+    it('should round all numbers of a vector up to the next whole number', () => {
+      const vector = [0.02, 1.9, -1.4];
+
+      expect(ceilingVector(vector)).toEqual([1, 2, -1]);
+    });
+  });
+});
--- src/vectors.ts
+++ src/vectors.ts
@@ -1,3 +1,6 @@
+/* Note: these functions assume uniform size of
+ * all vectors. This is all our game needs */
+
 export const addVectors = <TVector extends number[]>(...vectors: TVector[]) =>
   vectors.reduce((acc, vector) => acc.map((n, i) => n + vector[i]) as TVector);
 
