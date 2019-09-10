2e675c39ba06d4cef66ff9209d7562b0edfb312d
James Wright <james@jamesswright.co.uk>
2019-09-10 21:42:07 +0100

    Fixes camera tests

--- src/rendering/__tests__/camera.test.ts
+++ src/rendering/__tests__/camera.test.ts
@@ -1,15 +1,15 @@
 import createProject2D from '../camera';
+
 import createTilePositionable, {
-  TILE_WIDTH,
-  TILE_HEIGHT,
+  tileSize, Point2D,
 } from '../../tilePositionable';
 
 describe('project2D', () => {
   it('should return a fixed-length tuple array of the positionable projected against the viewport', () => {
-    const project2D = createProject2D(TILE_WIDTH * 1000, TILE_HEIGHT * 500);
+    const project2D = createProject2D(tileSize.map((s, i) => s * i === 0 ? 1000 : 500) as Point2D);
     const tilePositionable = createTilePositionable(1, 1, 4, 4);
     const projectedValues = project2D(tilePositionable);
 
-    expect(projectedValues).toEqual([0.31329, 0.129605, 1.25316, 0.51842]);
+    expect(projectedValues).toEqual([17.7, 8.05, 70.8, 32.2]);
   });
 });
