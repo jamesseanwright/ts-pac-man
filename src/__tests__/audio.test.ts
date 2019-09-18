f5a560502d09df909058d897aed0e3bcfac8f87f
James Wright <james@jamesswright.co.uk>
2019-09-18 08:45:35 +0100

    WIP audio player

---
+++ src/__tests__/audio.test.ts
@@ -0,0 +1,7 @@
+import { audioPlayerCreator } from '../audio';
+
+describe('createAudioPlayer', () => {
+  it('should create an audio player function that fetches the specified tracks and allows them to be played', () => {
+
+  });
+});
---
+++ src/audio.ts
@@ -0,0 +1,37 @@
+type Context = Pick<
+  AudioContext,
+  | 'createBufferSource'
+  | 'decodeAudioData'
+  | 'destination'
+>;
+
+type PromiseMapCallback<TItem, TResolve> = (item: TItem) => Promise<TResolve>;
+
+const mapToPromises = <TItem, TResolve>(items: TItem[], cb: PromiseMapCallback<TItem, TResolve>) =>
+  Promise.all(items.map(cb));
+
+export const audioPlayerCreator = (fetch: typeof window.fetch, audioContext: Context) =>
+  async (paths: string[]) => {
+    const defaultBuffer = await audioContext.decodeAudioData(new ArrayBuffer(0));
+    const responses = await mapToPromises(paths, path => fetch(path));
+    const arrayBuffers = await mapToPromises(responses, res => res.arrayBuffer());
+    const audioBuffers = await mapToPromises(arrayBuffers, ab => audioContext.decodeAudioData(ab));
+    const tracks = new Map(paths.map((p, i) => [p, audioBuffers[i]]));
+
+    let currentSource: AudioBufferSourceNode;
+
+    const play = (path: string) => {
+      if (currentSource) {
+        currentSource.stop();
+        currentSource.disconnect(audioContext.destination);
+      }
+
+      currentSource = audioContext.createBufferSource();
+      currentSource.buffer = tracks.get(path) || defaultBuffer;
+      currentSource.start();
+    };
+
+    return play;
+  };
+
+export default audioPlayerCreator(window.fetch, new AudioContext());
